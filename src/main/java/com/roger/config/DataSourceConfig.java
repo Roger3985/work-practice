c7ea924ea31d040e20d84754076777ae674364ea
孫拯昌(Zhengchang Sun) <2407009@iisigroup.com>
2024-07-07 22:07:44 +0800

    fixed

--- src/main/java/com/roger/config/DataSourceConfig.java
+++ src/main/java/com/roger/config/DataSourceConfig.java
@@ -5,7 +5,6 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-
 import javax.sql.DataSource;
 
 @Configuration
