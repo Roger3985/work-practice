d6c59ee17b278dfa7f0964aa455c5c5def547b50
zhengchangsun <a0967067696@gmail.com>
2024-07-10 10:40:20 +0800

    one version the 7/10 change

--- src/main/java/com/roger/config/DataSourceConfig.java
+++ src/main/java/com/roger/config/DataSourceConfig.java
@@ -1,35 +1,22 @@
 package com.roger.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import javax.sql.DataSource;
+import org.springframework.core.annotation.Order;
 
+@Order(Integer.MIN_VALUE)
 @Configuration
-@PropertySource("classpath:application.yml")
 public class DataSourceConfig {
 
-    @Value("${spring.datasource.url}")
+    @Value("${mybatis.datasource.url}")
     private String url;
 
-    @Value("${spring.datasource.username}")
+    @Value("${mybatis.datasource.username}")
     private String username;
 
-    @Value("${spring.datasource.password}")
+    @Value("${mybatis.datasource.password}")
     private String password;
 
-    @Value("${spring.datasource.driver-class-name}")
+    @Value("${mybatis.datasource.driver-class-name}")
     private String driverClassName;
-
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder.create()
-                .url(url)
-                .username(username)
-                .password(password)
-                .driverClassName(driverClassName)
-                .build();
-    }
 }
