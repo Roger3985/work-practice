0f85f9457bd3cf1ab0504e8bc36882a4bdcd686b
zhengchangsun <a0967067696@gmail.com>
2024-07-17 19:11:33 +0800

    new version on zk practice

--- src/main/java/com/roger/user/viewmodel/EditUserViewModel.java
+++ src/main/java/com/roger/user/viewmodel/EditUserViewModel.java
@@ -9,6 +9,7 @@ import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 
+import java.util.List;
 import java.util.Map;
 
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
@@ -17,7 +18,7 @@ public class EditUserViewModel {
     @WireVariable
     private UserService userService;
 
-    private UserDto user;
+    private UserDto user = new UserDto();
 
     @Init
     public void Init(@ExecutionArgParam("user") UserDto user) {
@@ -26,11 +27,19 @@ public class EditUserViewModel {
 
     @Command
     public void saveUser() {
-        userService.upateUser(user);
-        // 保存後跳轉會員詳情頁面或包含頁面
-        Executions.createComponents("~./zul/user/userDetailPage.zul", null, Map.of("user", user));
-    }
 
+        // 更新會員
+        userService.upateUser(user);
+
+        // 重新加載會員列表
+        List<UserDto> users = userService.findAllUsers();
+
+        // 將更新後的會員列表傳遞到會員列表頁面
+        Executions.getCurrent().getDesktop().setAttribute("users", users);
+
+        // 保存後跳轉會員詳情頁面或包含頁面
+        Executions.sendRedirect("~./zul/user/usersPage.zul");
+    }
     public UserDto getUser() {
         return user;
     }
